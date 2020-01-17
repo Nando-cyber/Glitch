7f6673ca13f6559fb95b8572cb58cce070df3dfb
Santolo Mutone <56079259+thelegendsGT@users.noreply.github.com>
2020-01-17 18:50:15 +0100

    update ordine

--- Work in Progress/CodeServlet/com/glitch/demo/model/dao/OrdineDAO.java
+++ Work in Progress/CodeServlet/com/glitch/demo/model/dao/OrdineDAO.java
@@ -1,8 +1,8 @@
-package com.glitch.demo.model.dao;
+package model.dao;
 
 import java.util.List;
 
-import com.glitch.demo.model.bean.Ordine;
+import model.bean.Ordine;
 
 public interface OrdineDAO {
 
