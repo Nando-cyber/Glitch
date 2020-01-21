4f9d11bd7034d96caed4083dd3c7a7cb663f5633
Santolo Mutone <56079259+thelegendsGT@users.noreply.github.com>
2020-01-21 12:11:08 +0100

    GestioneRuoloServlet testata e funzionante

--- Work in Progress/CodeServlet/src/controller/GestioneRuoloServlet.java
+++ Work in Progress/CodeServlet/src/controller/GestioneRuoloServlet.java
@@ -40,21 +40,22 @@ public class GestioneRuoloServlet extends HttpServlet {
 		// il ruolo dalla request
 		String operazione = request.getParameter("operazione");
 		Utente u = uDAO.retriveByUsername(request.getParameter("userUtente"));
-		String ruolo = request.getParameter("ruolo");
-
-		//Si verifica che il formato di ruolo sia corretto altrimenti si lancia un'eccezione
-		if(!ValidazioneUtente.checkRuolo(ruolo)) {
-			throw new MyServletException("Formato Ruolo non corretto");
-		}
+		
 
 		//si verifica che "operazione" sia un "aggiunta" o "rimozione"
 		if(operazione.equalsIgnoreCase("aggiunta")) {
 
+			String ruolo = request.getParameter("ruolo");
+			//Si verifica che il formato di ruolo sia corretto altrimenti si lancia un'eccezione
+			if(!ValidazioneUtente.checkRuolo(ruolo)) {
+				throw new MyServletException("Formato Ruolo non corretto");
+			}
+			
 			//Se � un'aggiunta, si assegna il ruolo all'utente selezionato
 			u.setRuolo(ruolo);
 
 			//Se l'assegnamento avviene con successo si aggiorna l'utente nel DB
-			uDAO.updateUtente(u);
+			uDAO.updateRuoloUtente(u);
 			//Si ritornano da DB tutti gli utenti presenti
 			List<Utente> utenti = uDAO.retriveAllUtenti();
 			//Si settano nella request in modo da poterli mostrare nella view
@@ -65,7 +66,7 @@ public class GestioneRuoloServlet extends HttpServlet {
 			// Se invece � una rimozione, si procede a rimuovere il ruolo dall'utente
 			u.removeRuolo();
 			//Si aggiorna l'utente
-			uDAO.updateUtente(u);
+			uDAO.updateRuoloUtente(u);
 			//Si ritornano da DB tutti gli utenti presenti
 			List<Utente> utenti = uDAO.retriveAllUtenti();
 			//Si settano nella request in modo da poterli mostrare nella view
@@ -82,7 +83,7 @@ public class GestioneRuoloServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		doGet(request, response);
 	}
--- Work in Progress/CodeServlet/src/controller/Test/GestioneRuoloServletTest.java
+++ Work in Progress/CodeServlet/src/controller/Test/GestioneRuoloServletTest.java
@@ -2,23 +2,79 @@ package controller.Test;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import controller.GestioneProfiloServlet;
+import controller.GestioneRuoloServlet;
+import model.bean.Utente;
+import model.dao.UtenteDAO;
+import model.dao.UtenteDB;
 
 public class GestioneRuoloServletTest {
 
+	static MockHttpServletRequest request;
+	static MockHttpServletResponse response;
+	static GestioneRuoloServlet servlet;
+	
 	@Before
-	public void setUp() throws Exception {
+	public void setUp() throws Exception {	
+		request= new MockHttpServletRequest();
+		response=new MockHttpServletResponse();
+		servlet= new GestioneRuoloServlet();
+		DatabaseHelper.initializeDatabase();
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		request=null;
+		response=null;
+		DatabaseHelper.resetDatabase();
 	}
 
 	@Test
-	public void testGestioneRuoloServlet() {
-		fail("Not yet implemented");
+	public void testGestioneRuoloServletAggiunta() throws ServletException, IOException {
+		request.addParameter("operazione", "aggiunta");
+		request.addParameter("userUtente", "Ferdinando98");
+		request.addParameter("ruolo", "Account");
+		servlet.doPost(request,response);
+		
+		UtenteDAO uDao=new UtenteDB();
+		Utente user=uDao.retriveByUsername("Ferdinando98");
+		boolean result;
+		if(user.getRuolo().equals("Account"))
+			result=true;
+		else
+			result=false;
+		
+		assertTrue(result);
+		
+	}
+	
+	@Test
+	public void testGestioneRuoloServletRimozione() throws ServletException, IOException {
+		request.addParameter("operazione", "rimozione");
+		request.addParameter("userUtente", "Ferdinando98");
+		request.addParameter("ruolo", "Account");
+		servlet.doPost(request,response);
+		
+		UtenteDAO uDao=new UtenteDB();
+		Utente user=uDao.retriveByUsername("Ferdinando98");
+		boolean result;
+		if(user.getRuolo()==null)
+			result=true;
+		else
+			result=false;
+		
+		assertTrue(result);
+		
 	}
 
 }
