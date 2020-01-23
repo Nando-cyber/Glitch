f4ff10d8d4aa94a0ebaa966ba63234b62c25a288
Nando-cyber <56017534+Nando-cyber@users.noreply.github.com>
2020-01-23 02:02:25 +0100

    sql update

--- Work in Progress/database/inserimento.sql
+++ Work in Progress/database/inserimento.sql
@@ -1,4 +1,4 @@
-use Glitch;
+﻿use Glitch;
 
 /*Prodotto*/
 INSERT INTO Prodotto (id, immagine, prezzo, descrizione) 
@@ -21,7 +21,6 @@ INSERT INTO Prodotto (id, immagine, prezzo, descrizione)
 VALUES(6,"https://images-na.ssl-images-amazon.com/images/I/81quqk%2BA98L._SL1500_.jpg",54.99, "Call of Duty: Modern Warfare offre ai fan una storia avvincente, cruda, provocatoria e dall'intensità impareggiabile, che mette in evidenza la natura mutevole della guerra moderna.
 ");
 
-
 /*Videogiochi*/
 INSERT INTO Videogioco (prodottoId, nome, genere, piattaforma) VALUES(1, "Call of Duty: Modern Warfare","FPS", "PlayStation 4");
 
@@ -35,6 +34,7 @@ INSERT INTO Videogioco (prodottoId, nome, genere, piattaforma) VALUES(6, "Call o
 INSERT INTO Console (prodottoID, modello, casaProduttrice) VALUES(2, "PS4", "Playstation");
 
 INSERT INTO Console (prodottoID, modello, casaProduttrice) VALUES(5, "Switch", "Nintendo");
+
 /*Utente*/
 INSERT INTO Utente (username, email, cartaDiCredito, password, nome, cognome, provincia, cap, citta, via, numero, ruolo) 
 VALUES("Ferdinando98","nando998@libero.it",null,"Ferdinando98", "Ferdinando","Napolitano", "NA", 80030, "Visciano", "Galileo Galilei", 1, null);
@@ -43,10 +43,10 @@ INSERT INTO Utente (username, email, cartaDiCredito, password, nome, cognome, pr
 VALUES("Admin98","adminCat99@glitch.com",null,"admin98", "Ferdinando","Napolitano", "NA", 80030, "Visciano", "Galileo Galilei", 1, "Catalogo");
 
 INSERT INTO Utente (username, email, cartaDiCredito, password, nome, cognome, provincia, cap, citta, via, numero, ruolo) 
-VALUES("Nunzia98","adminCat99@glitch.com",null,"admin98", "Ferdinando","Napolitano", "NA", 80030, "Visciano", "Galileo Galilei", 1, "Account");
-
-INSERT INTO Utente (username, email, cartaDiCredito, password, nome, cognome, provincia, cap, citta, via, numero, ruolo) 
-VALUES("Paki989","adminCat99@glitch.com",null,"admin98", "Ferdinando","Napolitano", "NA", 80030, "Visciano", "Galileo Galilei", 1, "Assistenza");
-
-INSERT INTO Utente (username, email, cartaDiCredito, password, nome, cognome, provincia, cap, citta, via, numero, ruolo) 
 VALUES("botGlitch","generAutoMail@live.com",null,"botGlitch99", "bot","bot", "GL", 00000, "Glitch", "Glitch", 1, null);
+
+INSERT INTO Utente (username, email, cartaDiCredito, password, nome, cognome, provincia, cap, citta, via, numero, ruolo) 
+VALUES("Nunzia98","nunzia98@libero.it",null,"Nunzia98", "Nunzia","Elefante", "NA", 80030, "Visciano", "Galileo Galilei", 1, "Assistenza");
+
+INSERT INTO Utente (username, email, cartaDiCredito, password, nome, cognome, provincia, cap, citta, via, numero, ruolo) 
+VALUES("Sandro98","sandro98@glitch.com",null,"Sandro98", "Sandro","Mutone", "NA", 80030, "Visciano", "Galileo Galilei", 1, "Account");
